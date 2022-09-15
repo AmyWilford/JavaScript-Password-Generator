fe5b867c318cc1999732c14b3e831accba8cf4a6
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-15 17:07:47 -0400

    consolidated upper and lowercase logic

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 var generateBtn = document.querySelector("#generate");
 // Function for password prompts
 
-// let startAgain= confirm ('Invalid entry, do you want to try again from the start?');
+// NOT WORKING RIGHT NOW let startAgain= confirm ('Invalid entry, do you want to try again from the start?');
 // if(startAgain) {
 //   passwordPrompts()
 // } else {
 //   alert('OK we can try again another day. Bye!')
 // }
 let passLength;
-
+// Function to determine password length
 let lengthPrompt = function() {
    passLength = prompt ('How long would you like your password to be (select a number between 8 and 128 characters');
   if (passLength >=8 && passLength<=128) {
@@ -19,17 +19,25 @@ let lengthPrompt = function() {
     alert('Invalid Entry. Start again')
   }
 }
+// function to determine inclusion of lowercase letters
 let lowercasePrompt = function(){
+  let uppercase='';
+  let lowercase='';
   let lowercaseR = prompt('Do you want to include lowercase letters (Y or N?').toUpperCase();
-  if(lowercaseR ==='Y') {
-    console.log(lowercaseR);
-  } else if (lowercaseR==='N') {
-      console.log(lowercaseR);
+  if (lowercaseR==='N') {
+    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    console.log(uppercase);
+    console.log('uppercase only')
+  } else if (lowercaseR ==='Y') {
+    lowercase ='abcdefghijklmnopqrstuvwxyz';
+    console.log(lowercase);
+    uppercasePrompt();
+    console.log('ask about uppercase')
   } else {
     alert('Invalid Entry. Start again')
   }
 }
-
+// 
 let uppercasePrompt = function(){
   let uppercaseR = prompt('Do you want to include uppercase letters (Y or N?)').toUpperCase();
   if(uppercaseR ==='Y') {
@@ -40,6 +48,7 @@ let uppercasePrompt = function(){
     alert('Invalid Entry. Start again')
   }
 }
+// Function to include numbers
 let numericPrompt = function() {
   let numberR = prompt('Do you want to include numbers in your password').toUpperCase();
   if(numberR ==='Y') {
@@ -50,8 +59,9 @@ let numericPrompt = function() {
     alert('Invalid Entry. Start again')
   }
 }
+// Function to include special characters
 let specialCharPrompt = function() {
-  let charR = prompt('Do you want to include numbers in your password').toUpperCase();
+  let charR = prompt('Do you want to include special characters in your password').toUpperCase();
   if(charR ==='Y') {
     console.log('yes to characters');
   } else if (charR==='N') {
@@ -61,25 +71,27 @@ let specialCharPrompt = function() {
   }
 }
 
-function passwordPrompts() {
+// Function to generate password
+let generatePassword= function(){
+  // Initial alert asking declaring that prompts are coming
   alert("Let's make you a secure password. First, let's determine your preferred password criteria.")
+    
+  // Password prompts in order
     lengthPrompt();
     lowercasePrompt();
-    uppercasePrompt();
+    // uppercasePrompt();
     numericPrompt();
     specialCharPrompt();
-    generatePassword();
     console.log('all questions asked');
+
+  // let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // for(let i=0; i<=passLength; i++){
+  //   let randomNumber = Math.floor(Math.random()*chars.Length);
+  //   password+=chars.substring(randomNumber,randomNumber+1)
+  //   console.log(password)
+  // }
 }
 
-let generatePassword= function(){
-  let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  for(let i=0; i<=passLength; i++){
-    let randomNumber = Math.floor(Math.random()*chars.Length);
-    password+=chars.substring(randomNumber,randomNumber+1)
-    console.log(password)
-  }
-}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -91,4 +103,4 @@ function writePassword() {
 
 // Add event listener to generate button - will call writePassword function
 // AMY REPLACE FUNCTION WITH writePassword
-generateBtn.addEventListener("click", passwordPrompts);
+generateBtn.addEventListener("click", writePassword);
