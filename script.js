000f702392c8ddebc21573d7ec14205b838f0741
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-15 20:15:36 -0400

    password generating in console

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 var generateBtn = document.querySelector("#generate");
 
 // Declare all password component variables
+let randomPassword='';
 let passLength;
 let lowercase ='abcdefghijklmnopqrstuvwxyz';
 let uppercase=lowercase.toUpperCase();
-let number;
-let specialChar;
+let number='1,2,3,4,5,6,7,8,9,0';
+let specialChar='!@#$%^&*()';
+let allChars = number.concat(lowercase,uppercase,specialChar);
 
 // Function to choose to stop playing.
 let keepGoing = function(r){
@@ -23,7 +25,7 @@ alert('OK we can try again another day. Bye!');
 // Function to determine password length
 let lengthPrompt = function() {
    passLength = prompt ('How long would you like your password to be (select a number between 8 and 128 characters');
-  if (typeof passLength =='number' && passLength >=8 && passLength<=128) {
+  if (/*typeof passLength =='number'&&*/ passLength >=8 && passLength<=128) {
     console.log(passLength);
   } else {
     keepGoing();
@@ -63,7 +65,8 @@ let numericPrompt = function() {
   if(numberR ==='Y') {
     console.log('yes to numbers');
   } else if (numberR==='N') {
-      console.log('no numbers');
+    number='';
+    console.log('no numbers');
   } else {
     keepGoing();
   }
@@ -74,43 +77,39 @@ let specialCharPrompt = function() {
   if(charR ==='Y') {
     console.log('yes to characters');
   } else if (charR==='N') {
+    specialChar='';
       console.log('no characters');
   } else {
     keepGoing();
   }
 }
-
+function createPassword(y){
+  y=y-1;
+ for(let i=0; i<=y; i++){
+   let randomPasswordNumnber = Math.floor(Math.random()*allChars.length-1);
+   randomPassword +=allChars.substring(randomPasswordNumnber, randomPasswordNumnber+1)
+ }
+}
 // Function to generate password
 let generatePassword= function(){
-
-  // Initial alert asking declaring that prompts are coming
   alert("Let's make you a secure password. First, let's determine your preferred password criteria.")
-    
-  // Password prompts in order
     lengthPrompt();
     lowercasePrompt();
-    // uppercasePrompt();
     numericPrompt();
     specialCharPrompt();
-    console.log('password')
-  // let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // for(let i=0; i<=passLength; i++){
-  //   let randomNumber = Math.floor(Math.random()*chars.Length);
-  //   password+=chars.substring(randomNumber,randomNumber+1)
-  //   console.log(password)
-  // }
-};
+    createPassword(passLength)
+    console.log(randomPassword);
+  };
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   console.log('Password');
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  console.log('Password');
+  passwordText.value = password;
 
-//   passwordText.value = password;
-
-// }
+}
 
 // Add event listener to generate button - will call writePassword function
 // AMY REPLACE FUNCTION WITH writePassword
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
